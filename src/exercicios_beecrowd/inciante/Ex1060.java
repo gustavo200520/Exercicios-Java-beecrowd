d81791f0ce3f4dc08baf40c346bbd0dfe8e200a7
Gustavo Matos Barbosa <gustavomatosbarbosa2@gmail.com>
2026-03-14 19:53:00 -0300

    alteracoes

--- src/exercicios_beecrowd/inciante/Ex1060.java
+++ src/exercicios_beecrowd/inciante/Ex1060.java
@@ -1,5 +1,29 @@
 package exercicios_beecrowd.inciante;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Ex1060 {
 
+    public static void main(String[] args) throws IOException {
+        Scanner teclado = new Scanner(System.in);
+        int tamanho=6;
+        int contador_numeros = 0;
+        float [] numero = new float[tamanho];
+        for(int cont=0;cont<tamanho; cont++) {
+
+            numero[cont] = teclado.nextFloat();
+        }
+
+
+        for(int cont=0;cont<tamanho;cont++) {
+
+            if(numero[cont]>0) {
+                contador_numeros++;
+            }
+
+        }
+        System.out.printf("%d valores positivos\n",contador_numeros);
+
+    }
 }
